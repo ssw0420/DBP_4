ef311adda207c1c2f471693fc7c8237a96fe79fd
ssw0420 <tmddn5645@gmail.com>
2024-12-03 11:11:29 +0900

    setting
    setting

---
+++ tpj/src/main/java/com/dbp/tpj/TpjApplication.java
@@ -0,0 +1,13 @@
+package com.dbp.tpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpjApplication.class, args);
+	}
+
+}
